f6fe1ec7a63f466aef0e4a08b8030bb93f7fb9ce
Mrkol <roma.sandu@gmail.com>
2014-07-12 22:35:25 +0400

    Fixed tabs being \t instead of     .

--- src/com/technoworks/fusionmonitor/view/widgets/WidgetGraph2.java
+++ src/com/technoworks/fusionmonitor/view/widgets/WidgetGraph2.java
@@ -19,42 +19,42 @@ import java.util.Queue;
  */
 public class WidgetGraph2 extends Widget
 {
-	public static final String TYPE = "Graph2";
+    public static final String TYPE = "Graph2";
 
-	public ArrayList<Double> values = new ArrayList<Double>();
-	private Paint backgroundPaint = new Paint();
-	private RectF graphBounds = new RectF();
+    public ArrayList<Double> values = new ArrayList<Double>();
+    private Paint backgroundPaint = new Paint();
+    private RectF graphBounds = new RectF();
 
-	public WidgetGraph2(Context context)
-	{
-		super(context);
+    public WidgetGraph2(Context context)
+    {
+        super(context);
 
-		this.backgroundPaint.setColor(Color.BLACK);
-		this.graphBounds.set(PADDING_DP + 8,
-							 PADDING_DP + 8,
-							 DEFAULT_SIZE[0] * MonitorActivity.APPROXIMATE_CELL_SIZE_DP * mMonitorActivity.mScreenDensity - (PADDING_DP * 2) - 8,
-							 DEFAULT_SIZE[1] * MonitorActivity.APPROXIMATE_CELL_SIZE_DP * mMonitorActivity.mScreenDensity - (PADDING_DP * 2) - 8);
-	}
+        this.backgroundPaint.setColor(Color.BLACK);
+        this.graphBounds.set(PADDING_DP + 8,
+                             PADDING_DP + 8,
+                             DEFAULT_SIZE[0] * MonitorActivity.APPROXIMATE_CELL_SIZE_DP * mMonitorActivity.mScreenDensity - (PADDING_DP * 2) - 8,
+                             DEFAULT_SIZE[1] * MonitorActivity.APPROXIMATE_CELL_SIZE_DP * mMonitorActivity.mScreenDensity - (PADDING_DP * 2) - 8);
+    }
 
-	@Override
-	protected void onResized()
-	{
-		if(this.getLayoutParams() != null)
-		{
-			this.graphBounds.set(PADDING_DP + 8,
-								 PADDING_DP + 8,
-								 this.getLayoutParams().width  - (PADDING_DP * 2) - 8,
-								 this.getLayoutParams().height - (PADDING_DP * 2) - 8);
-		}
-	}
+    @Override
+    protected void onResized()
+    {
+        if(this.getLayoutParams() != null)
+        {
+            this.graphBounds.set(PADDING_DP + 8,
+                                 PADDING_DP + 8,
+                                 this.getLayoutParams().width  - (PADDING_DP * 2) - 8,
+                                 this.getLayoutParams().height - (PADDING_DP * 2) - 8);
+        }
+    }
 
-	@Override
-	protected void onDraw(Canvas canvas)
-	{
-		canvas.save();
-		{
-			canvas.drawRect(graphBounds, backgroundPaint);
-		}
-		canvas.restore();
-	}
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        canvas.save();
+        {
+            canvas.drawRect(graphBounds, backgroundPaint);
+        }
+        canvas.restore();
+    }
 }
